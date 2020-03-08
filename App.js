9e8c2bea94ef6f36eaf33b8b2de0c02d4b3def3a
Leila <nb.leila10@gmail.com>
2020-03-07 20:04:38 -0600

    Setup redux

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import Thunk from "redux-thunk";
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import PlacesReducer from "./store/reducers/places";
+
+const rootReducer = combineReducers({
+  places: PlacesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(Thunk));
 
 export default function App() {
-  return <PlacesNavigator />;
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
 }
---
+++ store/reducers/places.js
@@ -0,0 +1,10 @@
+const initialState = {
+  places: []
+};
+
+export default PlacesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

e8266e95cf056104f0dcbb459f4f70f41244a1cb
Leila <nb.leila10@gmail.com>
2020-03-07 19:55:05 -0600

    Start implementing the form to add a place

--- screens/NewPlace.js
+++ screens/NewPlace.js
@@ -1,14 +1,46 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TextInput, View, Text, ScrollView, Button } from "react-native";
+import Colors from "../constants/Colors";
 
 const NewPlace = () => {
+  const [placeName, setPlaceName] = useState("");
+
+  const titleChangeHandler = text => {
+    setPlaceName(text);
+  };
+
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>New Place Screen!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} value={placeName} onChangeText={titleChangeHandler} />
+        <Button title='Save Place' color={Colors.primary} onPress={savePlaceHandler} />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+NewPlace.navigationOptions = {
+  headerTitle: "Add Place"
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  input: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 export default NewPlace;

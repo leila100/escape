e77d10d8fe4beda5c53143bcdea972bb299c4e57
Leila <nb.leila10@gmail.com>
2020-03-09 21:45:02 -0500

    Store places in database

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,21 +1,23 @@
 import * as FileSystem from "expo-file-system";
 
+import { insertPlace } from "../../helpers/db";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, imageUri) => {
   return async dispatch => {
     const fileName = imageUri.split("/").pop();
-    console.log(fileName);
     const newPath = FileSystem.documentDirectory + fileName;
     try {
       await FileSystem.moveAsync({
         from: imageUri,
         to: newPath
       });
+      const dbResult = await insertPlace(title, newPath, "Dummy Address", 15.6, 12.3);
+      dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title, imageUri: newPath } });
     } catch (err) {
       console.log(err);
       throw err;
     }
-    dispatch({ type: ADD_PLACE, placeData: { title, newPath } });
   };
 };

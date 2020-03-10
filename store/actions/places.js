b0fe46fa77efac9d3ca53da32ab70fe85127e4bb
Leila <nb.leila10@gmail.com>
2020-03-09 20:39:56 -0500

    Store image uri in FileSystem

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,5 +1,21 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, imageUri) => {
-  return { type: ADD_PLACE, placeData: { title, imageUri } };
+  return async dispatch => {
+    const fileName = imageUri.split("/").pop();
+    console.log(fileName);
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+      await FileSystem.moveAsync({
+        from: imageUri,
+        to: newPath
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+    dispatch({ type: ADD_PLACE, placeData: { title, newPath } });
+  };
 };

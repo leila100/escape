e68f11544fc04e1fcd097eaf5395807682c0b1f9
Leila <nb.leila10@gmail.com>
2020-03-09 21:07:45 -0500

    Initialize SQlite database

--- App.js
+++ App.js
@@ -2,9 +2,20 @@ import React from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import Thunk from "redux-thunk";
+import { init } from "./helpers/db";
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
 import PlacesReducer from "./store/reducers/places";
+
+// initialize the database
+init()
+  .then(() => {
+    console.log("Initialized the database");
+  })
+  .catch(err => {
+    console.log("Initializing the database failed.");
+    console.log(err);
+  });
 
 const rootReducer = combineReducers({
   places: PlacesReducer

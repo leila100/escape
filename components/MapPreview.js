910e7f12739dcc96d32785af1221888c21acc8f0
Leila <nb.leila10@gmail.com>
2020-03-11 20:30:02 -0500

    Add a marker

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -7,7 +7,6 @@ const MapPreview = props => {
   let imagePreview;
   if (props.location) {
     imagePreview = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`;
-    console.log(imagePreview);
   }
   return (
     <TouchableOpacity onPress={props.onPress} style={{ ...styles.mapPreview, ...props.style }}>

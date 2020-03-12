f0b28c726f22edf3b0e14c52155eb759a763ad55
Leila <nb.leila10@gmail.com>
2020-03-11 20:59:19 -0500

    Save picked location

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button, ActivityIndicator, Alert } from "react-native";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
@@ -18,7 +18,13 @@ const verifyPermissions = async () => {
 
 const LocationPicker = props => {
   const [isFetching, setIsFetching] = useState(false);
-  const [pickedLocation, setPickerLocation] = useState();
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const mapPickedLocation = props.navigation.getParam("pickedLocation");
+
+  useEffect(() => {
+    if (mapPickedLocation) setPickedLocation(mapPickedLocation);
+  }, [mapPickedLocation]);
 
   const getLocationHandler = async () => {
     const hasPermission = await verifyPermissions();
@@ -26,7 +32,7 @@ const LocationPicker = props => {
     try {
       setIsFetching(true);
       const location = await Location.getCurrentPositionAsync({ timeout: 5000 });
-      setPickerLocation({ lat: location.coords.latitude, lng: location.coords.longitude });
+      setPickedLocation({ lat: location.coords.latitude, lng: location.coords.longitude });
     } catch (err) {
       Alert.alert("Could not fetch location!", "Please try again later or pick a location on the map.", [
         { text: "ok" }

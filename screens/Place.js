cbcc7f6a1ebdf4aba1df5f1022f1e38d499b1871
Leila <nb.leila10@gmail.com>
2020-03-12 17:02:19 -0500

    Display details screen

--- screens/Place.js
+++ screens/Place.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Image, ScrollView, View } from "react-native";
+import { useSelector } from "react-redux";
 
-const Place = () => {
+import MapPreview from "../components/MapPreview";
+import Colors from "../constants/Colors";
+
+const Place = props => {
+  const placeId = props.navigation.getParam("placeId");
+  const selectedPlace = useSelector(state => state.places.places.find(pl => pl.id === placeId));
+
   return (
-    <View>
-      <Text>Place Screen!</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+      <Image source={{ uri: selectedPlace.imageUri }} style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{selectedPlace.address}</Text>
+        </View>
+        <MapPreview location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }} style={styles.mapPreview} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -15,6 +28,41 @@ Place.navigationOptions = navData => {
     headerTitle: title
   };
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    minHeight: 300,
+    width: "100%",
+    backgroundColor: "#ccc"
+  },
+  locationContainer: {
+    marginVertical: 20,
+    width: "90%",
+    maxWidth: 350,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    backgroundColor: "white",
+    borderRadius: 10
+  },
+  addressContainer: {
+    padding: 20
+  },
+  address: {
+    color: Colors.primary,
+    textAlign: "center"
+  },
+  mapPreview: {
+    width: "100%",
+    maxWidth: 350,
+    height: 300,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10
+  }
+});
 
 export default Place;
